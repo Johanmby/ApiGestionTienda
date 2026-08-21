8d4c1e20c8194fbad01e62736d7f602884f10dde
Johanmby <johanmby@gmail.com>
2026-08-21 13:37:32 -0500

    add: Agregando dependencias del swagger al proyecto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Configuración de la documentación Swagger
+  const config = new DocumentBuilder()
+    .setTitle('API Tienda Virtual')
+    .setDescription('Backend para gestión de productos, autenticación y roles')
+    .setVersion('1.0')
+    .addBearerAuth() // Habilita autenticación con Tokens JWT
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
+  await app.listen(3000);
 }
 bootstrap();
