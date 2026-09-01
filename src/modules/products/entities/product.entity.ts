b7fded53e91fe225d1fda9b344805f6c08f3e6e2
Johanmby <johanmby@gmail.com>
2026-09-01 11:40:34 -0500

    Se convirtieron las entidades de product y user en formato orm, es decir, se les agregaron los decoradores

--- src/modules/products/entities/product.entity.ts
+++ src/modules/products/entities/product.entity.ts
@@ -1,8 +1,21 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+@Entity('products')
 export class Product {
+  @PrimaryGeneratedColumn('uuid')
   id!: string;
+
+  @Column('text')
   name!: string;
+
+  @Column('text')
   description!: string;
+
+  @Column('numeric', { precision: 10, scale: 2 })
   price!: number;
+
+  @Column('int', { default: 0 })
   stock!: number;
+
+  @Column('boolean', { default: true })
   isActive!: boolean;
 }
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,13 +1,27 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
 export enum Role {
   USER = 'user',
   ADMIN = 'admin',
 }
 
+@Entity('users')
 export class User {
+  @PrimaryGeneratedColumn('uuid')
   id!: string;
+
+  @Column('text')
   name!: string;
+
+  @Column('text', { unique: true })
   email!: string;
+
+  @Column('text')
   password!: string;
+
+  @Column('text', { array: true, default: [Role.USER] })
   roles!: Role[];
+
+  @Column('boolean', { default: true })
   isActive!: boolean;
 }
